1e4a574e3a8b70ea63c320685618c4b699b8ada2
hirle <eric.hirlemann@gmail.com>
2021-01-10 19:09:50 +0100

    Coverage

--- api/src/__tests__/GetLatestMailsRequestHandler.test.ts
+++ api/src/__tests__/GetLatestMailsRequestHandler.test.ts
@@ -3,33 +3,98 @@ import { Request, Response} from 'express-serve-static-core';
 import { NextFunction } from 'express';
 import DbConnector from '../DbConnector';
 import GetLatestMailsRequestHandler from '../GetLatestMailsRequestHandler';
+import { Mail } from '@mail-in-memory/model';
+import { doesNotMatch } from 'assert';
+import { DNSPacket } from '@homebridge/ciao/lib/coder/DNSPacket';
 
 jest.mock('../DbConnector');
 
 describe('GetLatestMailsRequestHandler', () => {
 
-    it('should return mails with a 200 status code', ()=>{
+    beforeEach( () => {
+        jest.clearAllMocks();
+    });
+
+    it('should return mails with a 200 status code', done =>{
+
+        const loveMail = [
+            new Mail(
+                'romeo@shakespeare.org',
+                'juliet@shakespeare.org',
+                'Love letter',
+                'I love you so much',
+                new Date()
+                )
+        ]
+
         const mockedObjConnector = new DbConnector({filename: 'foo.bar'});
+        mockedObjConnector.getLatestMails = jest.fn().mockResolvedValueOnce(loveMail);
+
+        const underTest: (req: Request, res: Response, next: NextFunction) => void = GetLatestMailsRequestHandler.create(mockedObjConnector);
+
+        const mockNextFonction = jest.fn();
+        const testReq: any = {  params: {}, query: { count: 42 } };
+        const mockStatus = jest.fn();
+        const mockSend = jest.fn().mockImplementationOnce( payload => {
+            expect(payload).toEqual(loveMail);
+            expect( mockStatus ).toHaveBeenCalledWith(200);
+            expect( mockSend ).toHaveBeenCalled();
+
+            done();
+        });
+        mockStatus.mockImplementationOnce( () => ({ send: mockSend }) );
+        const testRes: any = { status: mockStatus };
+
+        underTest( testReq, testRes, mockNextFonction );
+
+        const mockDbConnector  = mocked(DbConnector,true);
+        expect(mockDbConnector.mock.instances[0].getLatestMails).toHaveBeenCalledWith(42);
+    });
+
+    it('should call next in case of error', done =>{
+
+        const mockedObjConnector = new DbConnector({filename: 'foo.bar'});
+        mockedObjConnector.getLatestMails = jest.fn().mockRejectedValueOnce(new Error('rainy day'));
+
+        const underTest: (req: Request, res: Response, next: NextFunction) => void = GetLatestMailsRequestHandler.create(mockedObjConnector);
+
+        const testReq: any = {  params: {}, query: {} };
+        const mockStatus = jest.fn();
+        const testRes: any = { status: mockStatus };
+        const mockNextFonction = jest.fn().mockImplementationOnce( err => {
+
+            expect(err).toBeInstanceOf(Error);
+            expect(mockStatus).not.toHaveBeenCalled();
+
+            done();
+        });
+
+        underTest( testReq, testRes, mockNextFonction );
+
+    });
+
+    it('should use default count if none provided', done =>{
+        const mockedObjConnector = new DbConnector({filename: 'qux.quux'});
         mockedObjConnector.getLatestMails = jest.fn().mockResolvedValueOnce([]);
 
         const underTest: (req: Request, res: Response, next: NextFunction) => void = GetLatestMailsRequestHandler.create(mockedObjConnector);
 
-
         const mockNextFonction = jest.fn();
-        const testReq: any = {  params: {}, query: {} };
+        const testReq: any = {  params: {}, query: { } };
         const mockStatus = jest.fn();
         const mockSend = jest.fn().mockImplementationOnce( payload => {
             expect(payload).toEqual([]);
-            expect( mockStatus ).toHaveBeenCalledWith(200);
+            expect( mockStatus ).toHaveBeenCalled();
             expect( mockSend ).toHaveBeenCalled();
+
+            done();
         });
         mockStatus.mockImplementationOnce( () => ({ send: mockSend }) );
-        const testRep: any = { status: mockStatus };
+        const testRes: any = { status: mockStatus };
 
-        underTest( testReq, testRep, mockNextFonction );
+        underTest( testReq, testRes, mockNextFonction );
 
-        const mockDbConnector  = mocked(DbConnector,true);
-        expect(mockDbConnector.mock.instances[0].getLatestMails).toHaveBeenCalled();
-
+        const mockDbConnector  = mocked(DbConnector);
+        expect(mockDbConnector.mock.instances[0].getLatestMails).toHaveBeenCalledWith(GetLatestMailsRequestHandler.defaultCount);
     });
 });

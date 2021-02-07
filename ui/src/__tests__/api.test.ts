f9ac043c8282884d18a76e2bfecdba2735e0cf63
hirle <eric.hirlemann@gmail.com>
2021-02-07 11:03:48 +0100

    Improve coverage

---
+++ ui/src/__tests__/api.test.ts
@@ -0,0 +1,54 @@
+import fetchMock from 'fetch-mock-jest';
+import * as api from '../api';
+import { DateTime, Duration } from 'luxon';
+
+describe('api', () => {
+
+    const loveMail =  {
+        fromAddress: 'romeo@shakespeare.tragedy.org',
+        toAddress: 'juliet@shakespeare.tragedy.org',
+        subject: 'Love letter',
+        body: 'I love you',
+        mailTimestamp: new Date()
+    }
+
+    afterEach(() => fetchMock.mockReset())
+    
+    it('should call /api/mails/latest', () => {
+
+        fetchMock.once({url:'/api/mails/latest', query: {count:10}, method: 'GET'}, [loveMail]);
+
+        return api.GetLastEmails(10).then(mails => {
+            expect(mails).toMatchObject([loveMail]);
+            expect(fetchMock.called('/api/mails/latest?count=10')).toBeTruthy();
+            expectCommonOptions(fetchMock.lastOptions());
+        });
+    });
+
+    it('should call /api/mails/for', () => {
+
+        fetchMock.once({url:'/api/mails/for/PT3H', method: 'GET'}, [loveMail]);
+
+        return api.GetEmailsFor(Duration.fromObject({hours:3})).then(mails => {
+            expect(mails).toMatchObject([loveMail]);
+            expectCommonOptions(fetchMock.lastOptions());
+        });
+    });
+
+    it('should call /api/mails/since', () => {
+
+        fetchMock.once(
+            {url:'/api/mails/since/1984-01-24T00:00:00.000+01:00', method: 'GET'},
+            [loveMail]
+            );
+
+        return api.GetEmailsSince(DateTime.fromObject({year:1984,month:1,day:24})).then(mails => {
+            expect(mails).toMatchObject([loveMail]);
+            expectCommonOptions(fetchMock.lastOptions());
+        });
+    });
+
+    function expectCommonOptions(options: any) {
+        expect(options.headers).toMatchObject({ Accept: 'application/json'});
+    }   
+});
--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,4 +1,5 @@
 import { Mail } from '@mail-in-memory/model'
+import { DateTime, Duration } from 'luxon';
 
 function checkStatus(response: Response) {
     if (response.status >= 200 && response.status < 300) {
@@ -19,15 +20,26 @@ const defaultOptions: RequestInit = {
     }
 };
 
-export function GetLastEmails(): Promise<Mail[]> {
-const url = `/api/mails/latest`;
-return fetch(url.toString(), defaultOptions).then(checkStatus)
-    .then(responseJson => {
-    if( Array.isArray(responseJson) ) {
-        return responseJson.map( elt => Mail.create(elt));
-    } else {
-        throw new Error('Expected an array, got ' + responseJson.toString())
-    }
-    });
+export function GetLastEmails(count: number): Promise<Mail[]> {
+    return fetchMails( `/api/mails/latest?count=${count.toString()}` ); 
+}
+
+export function GetEmailsFor(duration: Duration ): Promise<Mail[]> {
+    return fetchMails( `/api/mails/for/${duration.toISO()}` ); 
+}
+
+export function GetEmailsSince(date: DateTime ): Promise<Mail[]> {
+    return fetchMails( `/api/mails/since/${date.toISO()}` ); 
+}
+
+function fetchMails(path: string) : Promise<Mail[]>{
+    return fetch(path, defaultOptions).then(checkStatus)
+        .then(responseJson => {
+            if( Array.isArray(responseJson) ) {
+                return responseJson.map( elt => Mail.create(elt));
+            } else {
+                throw new Error('Expected an array, got ' + responseJson.toString())
+            }
+        });
 }
 

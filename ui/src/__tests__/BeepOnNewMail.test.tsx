fb7d52248f82180661404ef1314495408b5135cd
hirle <eric.hirlemann@gmail.com>
2021-02-28 11:22:25 +0100

    Improve coverage

---
+++ ui/src/__tests__/BeepOnNewMail.test.tsx
@@ -0,0 +1,38 @@
+import renderer from "react-test-renderer";
+import SocketIo from "../SocketIo";
+import BeepOnNewMail from "../BeepOnNewMail";
+import { Mail } from "@mail-in-memory/model";
+import { SocketMessages } from "@mail-in-memory/model";
+import MockDate from 'mockdate'
+
+describe("BeepOnNewMail", () => {
+
+  afterEach( () => {
+    MockDate.reset();
+  });  
+
+  it("should update date on new mail", () => {
+
+    const fixedDate = new Date('2021-02-28T09:33:30Z')
+    MockDate.set(fixedDate);
+
+    const mySocketIo = new SocketIo();
+    const component = renderer.create(<BeepOnNewMail socketIo={mySocketIo} />);
+    let initialTree = component.toJSON();
+    expect(initialTree).toMatchSnapshot();
+
+    const incomingMail = Mail.create({
+      messageId: "123-234-456-789",
+      fromAddress: "foo@email.org",
+      toAddress: "jest@test.mail-in-memory",
+      subject: "Cool",
+      body: "With jest, unit testing is so cool",
+      mailTimestamp: new Date("2021-02-28T09:33:22Z"),
+    });    
+    mySocketIo.notify(SocketMessages.NewMail, incomingMail);
+
+    let afterMail = component.toJSON();
+    expect(afterMail).toMatchSnapshot();
+
+  });
+});
